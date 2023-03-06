d7e6c2b7c25e8d034e5949ad892d18abf3480715
Trung Hieu <hieu@ltv.dev>
2023-03-06 10:44:53 +0700

    Set header

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const http = require('http');
 
 //create server
 const server = http.createServer((request, response) => {
-    response.write('Hello World!'); //render to browser -> localhost:3000 -> Hello World
+    response.setHeader('Content-Type', 'text/html'); 
+    response.write('<h1>Hello World!</h1>'); //render to browser -> localhost:3000 -> Hello World
     response.end();
 });
 
