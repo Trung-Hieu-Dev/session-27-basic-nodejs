833fd78f8452318eac6a8ab615ea8e4db7fb975c
Trung Hieu <hieu@ltv.dev>
2023-03-06 21:24:54 +0700

    Parsing incoming data with body-parser

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 //working with express.js
-const express = require('express');
+const express = require("express");
+
+//import library to parse data
+const bodyParser = require("body-parser");
 
 const app = express();
 
-//middleware
-app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html');
-    next(); //not done ->need to continue with next middleware below
-})
+//parse incoming data
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//middleware
+//middleware to set header
 app.use((req, res, next) => {
-    res.send('<h1>Hello World!</h1>');
+    res.setHeader("Content-Type", "text/html");
+    next(); //not done -> need to continue with next middleware below
 });
 
+//middleware to render
+app.use((req, res, next) => {
+    const userName = req.body.username || "Unknown User";
+    res.send(`
+        <h1>Hi ${userName}</h1>
+        <form method="POST" action="/">
+            <input name="username" type="text" />
+            <button type="submit">Send</button>
+        </form>`);
+});
+
+//port
 app.listen(3000);
--- pure-node.js
+++ pure-node.js
@@ -3,17 +3,20 @@ const http = require('http');
 
 //create server
 const server = http.createServer((request, response) => {
+    //received incoming data
     let body = [];
     request.on('data', (chunk) => {
         body.push(chunk);
     });
     request.on('end', () => {
+        //parse data
         body = Buffer.concat(body).toString();
         let userName = 'Unknown User';
         if (body) {
             userName = body.split('=')[1];
         }
 
+        //return data to render
         response.setHeader('Content-Type', 'text/html');
         response.write(`
             <h1>Hi ${userName}</h1>
