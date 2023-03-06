ccc4757d04b8a7f64a30d8426b39f2b8af3d87cd
Trung Hieu <hieu@ltv.dev>
2023-03-06 15:39:37 +0700

    Parsing incoming data

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ const http = require('http');
 
 //create server
 const server = http.createServer((request, response) => {
-    response.setHeader('Content-Type', 'text/html'); 
-    response.write('<h1>Hello World!</h1>'); //render to browser -> localhost:3000 -> Hello World
-    response.end();
+    let body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        let userName = 'Unknown User';
+        if (body) {
+            userName = body.split('=')[1];
+        }
+
+        response.setHeader('Content-Type', 'text/html');
+        response.write(`
+            <h1>Hi ${userName}</h1>
+            <form method="POST" action="/">
+                <input name="username" type="text" />
+                <button type="submit">Send</button>
+            </form>`);
+        response.end();
+    });
+
+    
 });
 
 server.listen(3000);
